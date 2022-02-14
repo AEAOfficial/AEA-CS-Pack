f5f53f8be483e4fc5fa16208f552660f77b327d2
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2022-02-14 05:40:23 -0500

    Added AEA Unlimited 1.0.8 for Community Votes

---
+++ GITHUBDESKTOP.bat
@@ -0,0 +1,32 @@
+:A
+@echo off
+set /p UPLOADMES="UploadMessage: "
+set /p ANSWER="Are you sure you want to Upload with this message? "%UPLOADMES%" (y/n)"
+
+if %ANSWER% == NUL GOTO A:
+if %ANSWER% == n GOTO A:
+if %ANSWER% == y GOTO B:
+:B
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo Testing for Changes
+echo TimeStamp: %time%
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+git fetch origin
+git pull origin
+timeout 10 > NUL
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo Uploading Changes
+echo TimeStamp: %time%
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo %UPLOADMES%
+git add .
+git commit -a -m "%UPLOADMES%"
+git push
+timeout 10 > NUL
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo  Changes Pushed
+echo TimeStamp: %time%
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo You may close or continue 
+pause
+GOTO A:
